b170a4ca658624fa661bfda7805d71df90c728ac
Ertuğrul Emre Ertekin <ee@ertek.in>
2014-02-24 10:58:54 +0200

    Create spammers.js

---
+++ spammers.js
@@ -0,0 +1,10 @@
+/*
+  
+  'timezone' : 'UTC'
+  
+*/
+
+var spamDB = { 
+  'BONUSFRST' : {'timestamp':1393232153, 'reason' : 'Phishing attempt' },
+  '905462058063', {'timestamp':1393232167, 'reason' : 'Hotel campaign' }
+}
